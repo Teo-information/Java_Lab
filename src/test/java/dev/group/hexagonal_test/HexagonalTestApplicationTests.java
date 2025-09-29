5efca7bc34c7b815f004f7e5006a63c38aef7b7e
Edward Imanol Lopez Cruz <imanolopez3103@gmail.com>
2025-09-29 18:58:30 -0500

    Inicialización del proyecto

---
+++ src/test/java/dev/group/hexagonal_test/HexagonalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.group.hexagonal_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
